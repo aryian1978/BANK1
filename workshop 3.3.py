ef7f69b6f1f54c95d2b05a647b10ccc9e7bee6e2
aryian1978 <43996721+aryian1978@users.noreply.github.com>
2018-10-09 16:08:42 +0100

    Add files via upload

---
+++ workshop 3.3.py
@@ -0,0 +1,64 @@
+Python 3.3.3 (v3.3.3:c3896275c0f6, Nov 18 2013, 21:18:40) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+type first number7
+7
+type second number3
+3
+the sum of two numbers is 10
+>>> ================================ RESTART ================================
+>>> 
+type first number1
+1
+type second number1
+1
+the sum of two numbers is 2
+>>> 
+>>> num = 4
+>>> num* = 2
+SyntaxError: invalid syntax
+>>> ================================ RESTART ================================
+>>> 
+type first number4num* = 2
+Traceback (most recent call last):
+  File "C:/Users/21291195/Desktop/workshop3.py", line 1, in <module>
+    mynumber = int(input("type first number"))
+ValueError: invalid literal for int() with base 10: '4num* = 2'
+>>> 
+>>> 
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+type first number6
+6
+type second number7
+7
+the sum of two numbers is 13
+num is 4
+num is 8
+>>> ================================ RESTART ================================
+>>> 
+type first number8
+8
+type second number6
+6
+the sum of two numbers is 14
+num is 4
+num is 8
+('num1=num+2', 8)
+>>> ================================ RESTART ================================
+>>> 
+type first number
+>>> ================================ RESTART ================================
+>>> 
+type first number6
+6
+type second number5
+5
+the sum of two numbers is 11
+num is 4
+num is 8
+('num1=num+2', 8)
+13
+>>> 
---
+++ workshop3.py
@@ -0,0 +1,14 @@
+mynumber = int(input("type first number"))
+print(mynumber)
+mynumber2 = int (input("type second number"))
+print (mynumber2)
+print ("the sum of two numbers is", (mynumber + mynumber2))
+num = 4
+print ("num is", num)
+num*=2
+print("num is", num)
+num1=num+2
+print(("num1=num+2",num))
+num1+=3
+print(num1)
+
